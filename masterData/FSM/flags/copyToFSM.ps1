c75e5826f0f657ca6526e3b2b54240bc14d21833
Clemens Brummer <clemens.brummer@gmail.com>
2023-04-05 23:50:12 +0200

    fix: copy flags to correct FSM folder

--- masterData/FSM/flags/copyToFSM.ps1
+++ masterData/FSM/flags/copyToFSM.ps1
@@ -1,4 +1,4 @@
-ls $env:LOCALAPPDATA\IceQueenUiShell\app-* | % { $fsm_app_dir = $_.fullname }
+ls $env:LOCALAPPDATA\FSManagerUiShell\app-* | % { $fsm_app_dir = $_.fullname }
 echo "FSM appdata directory: $fsm_app_dir"
 mkdir "$fsm_app_dir\Flags"
 ls *.png | % { echo "Copying $_ to $fsm_app_dir\Flags"; cp $_.fullname $fsm_app_dir\Flags }
