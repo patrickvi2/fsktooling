f30124c7b6347c27b53071384131881bffe1a2d1
Clemens Brummer <clemens.brummer@gmail.com>
2024-04-07 04:54:16 +0200

    refactor and cleanup

---
+++ fsklib/utils/common.py
@@ -0,0 +1,15 @@
+import unicodedata
+
+
+def normalize_string(s : str):
+    translation_table = str.maketrans('','',' -_.,')
+
+    # normalize unicode characters, remove characters, lowercase, ä -> ae, ö -> oe, ü -> ue
+    return (
+        unicodedata.normalize("NFC", unicodedata.normalize("NFD", s))
+        .translate(translation_table)
+        .casefold()
+        .replace("ä", "ae")
+        .replace("ö", "oe")
+        .replace("ü", "ue")
+    )
--- fsklib/utils/pb.py
+++ fsklib/utils/pb.py
@@ -1,31 +1,20 @@
-import requests
+from pathlib import Path
+from typing import Dict, List, Union
 import xml.etree.ElementTree as ET
-import unicodedata
 
-from typing import List
+import requests
 
-api_url = "https://deu-s.de/api/get_pb_for_skater?q="
-input_xml_file_path = "C:/SwissTiming/OVR/FSManager/Export/KBB24/ODF/DT_PARTIC_UPDATE_24-02-12_01-17-34.xml"
-is_debug = False
+from fsklib.utils.common import normalize_string
 
-# code
-tree = ET.parse(input_xml_file_path)
-root = tree.getroot()
-
-
-def normalize_string(s : str):
-    translation_table = str.maketrans('','',' -_.,')
-
-    # normalize unicode characters, remove characters, lowercase, ä -> ae, ö -> oe, ü -> ue
-    return unicodedata.normalize('NFC', unicodedata.normalize('NFD', s)).translate(translation_table).casefold().replace('ä', 'ae').replace('ö', 'oe').replace('ü', 'ue')
+API_URL = "https://deu-s.de/api/get_pb_for_skater?q="
 
 
 def get_pb_from_name(name: str):
-    response = requests.get(api_url + name)
+    response = requests.get(API_URL + name, timeout=5.0)
     return response.json()
 
 
-def parse_response(response: str) -> List[ET.Element]:
+def parse_response(response: Dict[str, str]) -> List[ET.Element]:
     json_keys_l1 = ["pb", "sb"]
     xml_tags_l1 = ["PB", "SB"]
     json_keys_l2 = ["short", "free", "total"]
@@ -33,52 +22,62 @@ def parse_response(response: str) -> List[ET.Element]:
 
     xml_entries = []
 
-    if set(json_keys_l1).intersection(resp.keys()): # has pb or sb key?
+    if set(json_keys_l1).intersection(response.keys()): # has pb or sb key?
         for json_key_l1, xml_tag_l1 in zip(json_keys_l1, xml_tags_l1):
             for json_key_l2, xml_tag_l2 in zip(json_keys_l2, xml_tags_l2):
                 try:
-                    attrib = {"Type": "ENTRY", 
-                            "Code": xml_tag_l1, 
+                    attrib = {"Type": "ENTRY",
+                            "Code": xml_tag_l1,
                             "Pos": xml_tag_l2,
-                            "Value": str(resp[json_key_l1][json_key_l2]["points"])}
-                except Exception as e:
+                            "Value": str(response[json_key_l1][json_key_l2]["points"])}
+                except Exception:
                     continue
                 xml_entries.append(ET.Element("EventEntry", attrib))
     return xml_entries
 
 
-for par in root.iter('Participant'):
-    name = par.get("GivenName") + " " + par.get("FamilyName")
-    sname = par.get("GivenName").lower() + par.get("FamilyName").lower()
-    sname = normalize_string(sname)
-    event = par.find("./Discipline/RegisteredEvent[EventEntry]")
-    if event:
-        resp = get_pb_from_name(sname)
-        entries = parse_response(resp)
-        if entries:
-            
-            # print matching names if not identical
-            if "meta" in resp:
-                if sname != resp["meta"]["slug"]:
-                    print(f"Found different name: {name} -> {resp['meta']['name']}")
-            elif is_debug:
-                print(f"found {name} -> {resp['meta']['name']}")
-
-            for entry in reversed(entries):
-                search_string = "./EventEntry"
-                for key in entry.attrib:
-                    if key == "Value":
-                        continue
-                    search_string += f"[@{key}='{entry.attrib[key]}']"
-                existing_entry = event.find(search_string)
-                if existing_entry is None:
-                    event.insert(0, entry)
+def update_statistics(input_odf_path: Union[str, Path]):
+    tree = ET.parse(input_odf_path)
+    root = tree.getroot()
+    for par in root.iter('Participant'):
+        name = str(par.get("GivenName", "") + " " + par.get("FamilyName", "")).strip()
+        sname = str(par.get("GivenName", "").lower() + par.get("FamilyName", "").lower()).strip()
+        sname = normalize_string(sname)
+        event = par.find("./Discipline/RegisteredEvent[EventEntry]")
+        if event:
+            resp = get_pb_from_name(sname)
+            entries = parse_response(resp)
+            if entries:
+                # print matching names if not identical
+                if "meta" in resp and "slug" in resp["meta"]:
+                    if sname != resp["meta"]["slug"]:
+                        if "name" in resp["meta"]:
+                            print(f"Found different name: {name} -> {resp['meta']['name']}")
+                        else:
+                            print(f"Found different name: {sname} -> {resp['meta']['slug']}")
                 else:
-                    existing_entry.set("Value", entry.get("Value"))
-        else:
-            print(f"Unable to find {name}")
+                    print(f"Unable to check name for {name}. No field \"meta\" in HTTP response.")
+                    if "name" in resp["meta"]:
+                        print(f"    Found {name} -> {resp['meta']['name']}")
 
-with open("odf.xml", "w", encoding="utf-8") as f:
-    t = ET.tostring(root, xml_declaration=True, encoding="unicode")
-    f.write(t)
+                for entry in reversed(entries):
+                    search_string = "./EventEntry"
+                    for key in entry.attrib:
+                        if key == "Value":
+                            continue
+                        search_string += f"[@{key}='{entry.attrib[key]}']"
+                    existing_entry = event.find(search_string)
+                    if existing_entry is None:
+                        event.insert(0, entry)
+                    else:
+                        existing_entry.set("Value", entry.get("Value", "-"))
+            else:
+                print(f"Unable to find {name}")
 
+    with open("odf.xml", "w", encoding="utf-8") as f:
+        t = ET.tostring(root, xml_declaration=True, encoding="unicode")
+        f.write(t)
+
+if __name__ == "__main__":
+    ODF = "C:/SwissTiming/OVR/FSManager/Export/CODE/ODF/DT_PARTIC_UPDATE_24-01-01_00-00-00.xml"
+    update_statistics(ODF)
